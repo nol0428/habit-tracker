e2ab212e37b11bd33dab994aab8a9b496f11e12f
Naing Oo Lwin <naingoolwin@gmail.com>
2025-10-21 01:05:33 +0900

    added urls

--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Habit
+
+def home(request):
+    habits = Habit.objects.all().order_by('created_at')
+    return render(request, 'habits/home.html', {'habits': habits})
