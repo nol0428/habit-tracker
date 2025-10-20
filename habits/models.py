cf84e50ec99a3b33822d22c4be3c8387b5a1b7f9
Naing Oo Lwin <naingoolwin@gmail.com>
2025-10-20 16:06:20 +0900

    Initial commit: Django Habit Tracker setup

---
+++ habits/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Habit(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
