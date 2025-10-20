981070449af206f18642e698fb8041c4324159b2
Naing Oo Lwin <naingoolwin@gmail.com>
2025-10-21 01:44:02 +0900

    add a new habit

--- habits/views.py
+++ habits/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect
 from .models import Habit
+from .forms import HabitForm
 
 def home(request):
     habits = Habit.objects.all().order_by('created_at')
-    return render(request, 'habits/home.html', {'habits': habits})
+    form = HabitForm()
+
+    if request.method == 'POST':
+        form = HabitForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    return render(request, 'habits/home.html', {'habits': habits, 'form': form})
