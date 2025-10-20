6ceaf06205c16167e94bbe20378bfa4119202218
Naing Oo Lwin <naingoolwin@gmail.com>
2025-10-21 01:34:29 +0900

    added home

--- habit_tracker/urls.py
+++ habit_tracker/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('habits.urls')),  # 👈 include app routes here
+    path('', include('habits.urls')),  # ✅ this one
 ]
