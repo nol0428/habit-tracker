efa1d834f929dd3cdcc7114f0437abcb5050ddb8
Naing Oo Lwin <naingoolwin@gmail.com>
2025-10-21 15:23:08 +0900

    addd css

--- habits/urls.py
+++ habits/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('mark_done/<int:habit_id>/', views.mark_done, name='mark_done'),
+    path('delete/<int:habit_id>/', views.delete_habit, name='delete_habit'),
 ]
--- habits/views.py
+++ habits/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render, redirect
-from .models import Habit
+from django.shortcuts import render, redirect, get_object_or_404
+from django.db.models import Max
+from .models import Habit, Record
 from .forms import HabitForm
 
+
 def home(request):
-    habits = Habit.objects.all().order_by('created_at')
+    habits = Habit.objects.annotate(last_completed=Max('records__completed_at')).order_by('created_at')
     form = HabitForm()
 
     if request.method == 'POST':
@@ -13,3 +15,14 @@ def home(request):
             return redirect('home')
 
     return render(request, 'habits/home.html', {'habits': habits, 'form': form})
+
+
+def mark_done(request, habit_id):
+    habit = get_object_or_404(Habit, id=habit_id)
+    Record.objects.create(habit=habit)
+    return redirect('home')
+
+def delete_habit(request, habit_id):
+    habit = get_object_or_404(Habit, id=habit_id)
+    habit.delete()
+    return redirect('home')
