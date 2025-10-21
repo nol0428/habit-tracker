2f2dd1efb34b2a6a7ede905dfdc9387948414cb5
Naing Oo Lwin <naingoolwin@gmail.com>
2025-10-21 12:31:48 +0900

    added bootstrap

--- habits/forms.py
+++ habits/forms.py
@@ -5,3 +5,9 @@ class HabitForm(forms.ModelForm):
     class Meta:
         model = Habit
         fields = ['name']
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter a new habit...'
+            })
+        }
